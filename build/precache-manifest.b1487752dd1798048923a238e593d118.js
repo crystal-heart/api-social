3c2334c1c28a74a811ee3f455861318aa8807a0b
vantt <vantt@vnext.com.vn>
2019-12-31 14:06:17 +0700

    add build

---
+++ build/precache-manifest.b1487752dd1798048923a238e593d118.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "914cb0ef4b3c781c5d9834647cfaded6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c7b58c1dd3da704a4a0",
+    "url": "/static/css/main.d3183f1a.chunk.css"
+  },
+  {
+    "revision": "4fe216b5eec57e0ed5e9",
+    "url": "/static/js/2.128727eb.chunk.js"
+  },
+  {
+    "revision": "5ec987632bc308bcd1222d7283dbdad9",
+    "url": "/static/js/2.128727eb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7c7b58c1dd3da704a4a0",
+    "url": "/static/js/main.c72f0f88.chunk.js"
+  },
+  {
+    "revision": "1675409b3ac93958d89a",
+    "url": "/static/js/runtime-main.30da00e1.js"
+  },
+  {
+    "revision": "8e89706942aa896adb17cba2417af9f6",
+    "url": "/static/media/close-icon.8e897069.svg"
+  },
+  {
+    "revision": "15b072615088f61dbed254cbbcead279",
+    "url": "/static/media/heart-solid.15b07261.svg"
+  },
+  {
+    "revision": "5e364adb3b5c0ca1cad2516b7e5db4d8",
+    "url": "/static/media/lock-icon.5e364adb.svg"
+  },
+  {
+    "revision": "61481528e8156b2d68032da4d0bfbc62",
+    "url": "/static/media/menu-icon.61481528.svg"
+  },
+  {
+    "revision": "00cc5a32b8b4e675ea6fa4f264352dad",
+    "url": "/static/media/remind-icon.00cc5a32.svg"
+  }
+]);
